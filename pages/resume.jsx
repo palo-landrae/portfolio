11f7b3d73402350c30ec7e4f26ce4245de45f3cd
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-02-21 12:53:12 +0100

    added unsaved files

---
+++ pages/resume.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Resume() {
+    return (
+        <div className="dark:bg-midnight bg-dawn h-full">
+            <Head>
+                <title>Resume</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
