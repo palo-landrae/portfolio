9233ba5ef26fb9c11a1b157e30612c57055504a8
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-02-22 13:12:00 +0100

    added footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,16 @@
 import "../styles/globals.css";
 import { ThemeWrapper } from "../context/theme";
 import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeWrapper>
-      <Navbar />
-      <Component {...pageProps} />
-    </ThemeWrapper>
-  );
+    return (
+        <ThemeWrapper>
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
+        </ThemeWrapper>
+    );
 }
 
 export default MyApp;
