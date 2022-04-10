a352944230485cea60dda8bd832c066a9cd7479d
Loui Andrae Palo <louiandraepalo@gmail.com>
2022-04-10 19:01:20 +0200

    :construction: chakra-ui-flashless

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,5 @@
-import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { FlashlessScript } from "chakra-ui-flashless";
 import theme from "../lib/theme";
 
 export default class Document extends NextDocument {
@@ -8,7 +8,7 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <FlashlessScript theme={theme} />
           <Main />
           <NextScript />
         </body>
